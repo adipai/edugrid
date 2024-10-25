84c44475aab0c3b9042254843fbbe8f2613b9a39
Deepak <deepak.rajendran123@gmail.com>
2024-10-25 16:01:25 -0400

    added some login pages

---
+++ frontend/src/pages/LoginHome.tsx
@@ -0,0 +1,19 @@
+import { Link } from 'react-router-dom';
+
+const LoginHome = () => {
+    return (
+        <div>
+            <h1>Login Home</h1>
+            <nav>
+                <ul>
+                    <li><Link to="/admin/login">1. Admin Login</Link></li>
+                    <li><Link to="/faculty/login">2. Faculty Login</Link></li>
+                    <li><Link to="/ta/login">3. Teaching Assitant Login</Link></li>
+                    <li><Link to="/student/login">4. Student Login</Link></li>
+                </ul>
+            </nav>
+        </div>
+    );
+};
+
+export default LoginHome;
---
+++ frontend/src/pages/TA/TALoginPage.tsx
@@ -0,0 +1,61 @@
+import axios from "axios";
+import { useState } from "react";
+
+const TALoginPage = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const role = "teaching assistant";
+
+  const handleSubmit = async (e: any) => {
+    e.preventDefault();
+    try {
+      const response = await axios.post('http://localhost:8000/login', {
+        email,
+        password,
+        role,
+      }, {headers: {
+        'Content-Type': 'application/json',
+      }, withCredentials: false});
+      console.log('Login successful:', response.data);
+      window.alert('Login successful');
+      // Handle success
+    } catch (error: any) {
+      if (error.response) {
+        console.error('Login failed:', error.response.data.message);
+      } else {
+        console.error('An error occurred:', error.message);
+      }
+    }
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+        <h3>Teaching Assitant Login</h3>
+      <div>
+        <label htmlFor="email">Email:</label>
+        <input
+          type="text"
+          id="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          required
+        />
+      </div>
+
+      <div>
+        <label htmlFor="password">Password:</label>
+        <input
+          type="password"
+          id="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          required
+        />
+      </div>
+
+      <button type="submit">Login</button>
+    </form>
+  );
+};
+
+export default TALoginPage;
