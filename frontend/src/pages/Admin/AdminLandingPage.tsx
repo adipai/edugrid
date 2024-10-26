d0c3108356304ec8064c8f9d13a5c9c7ff2dc4d5
Deepak <deepak.rajendran123@gmail.com>
2024-10-26 13:12:43 -0400

    merged admin page and app routing

--- frontend/src/pages/Admin/AdminLandingPage.tsx
+++ frontend/src/pages/Admin/AdminLandingPage.tsx
@@ -7,8 +7,8 @@ const AdminLandingPage = () => {
             <h1>Admin Dashboard</h1>
             <ul>
                 <li><Link to="/admin/create-faculty-account">1. Create a Faculty Account</Link></li>
-                <li><Link to="/create-etextbook">2. Create E-textbook</Link></li>
-                <li><Link to="/modify-etextbooks">3. Modify E-textbooks</Link></li>
+                <li><Link to="/admin/create-textbook">2. Create E-textbook</Link></li>
+                <li><Link to="/admin/modify-textbook">3. Modify E-textbooks</Link></li>
                 <li><Link to="/admin/create-active-course">4. Create New Active Course</Link></li>
                 <li><Link to="/admin/create-evaluation-course">5. Create New Evaluation Course</Link></li>
                 <li><Link to="/">6. Logout</Link></li>
