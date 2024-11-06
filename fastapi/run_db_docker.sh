a751810147488c3023fc009d6fbd7deb61123b16
Deepak <deepak.rajendran123@gmail.com>
2024-11-06 11:22:20 -0500

    remove deprecated backend files and configurations

--- fastapi/run_db_docker.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-docker run --name fastapi_mariadb -e MYSQL_ROOT_PASSWORD=root_password -e MYSQL_DATABASE=mydb -e MYSQL_USER=user -e MYSQL_PASSWORD=user_password -p 3306:3306 -d mariadb:latest
-
