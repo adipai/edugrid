9435d1cf53f05d43043e07217476b4468f109fa0
Aditya Pai <adipai16@gmail.com>
2024-11-12 17:58:22 -0500

    Delete db/versions directory

--- db/versions/db_04.sql
+++
@@ -1,5 +0,0 @@
--- Add sequence number to block, increasing order
-ALTER TABLE block ADD COLUMN sequence_no INT NOT NULL AUTO_INCREMENT UNIQUE;
-
--- Drop username column from student, not needed 
-ALTER TABLE student DROP COLUMN username;
